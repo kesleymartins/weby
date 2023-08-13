36c3b1fd343198d1612f0a18dfe8aafc1406a597
kesleymartins <kesley.martins@vivaldi.net>
2023-08-13 19:48:07 -0300

    create a base application with rack specifications

---
+++ app.rb
@@ -0,0 +1,13 @@
+class App
+  def call(env)
+    headers = {
+      'Content-Type' => 'text/html'
+    }
+
+    response = [
+      '<h1>Hello World</h1>'
+    ]
+
+    [200, headers, response]
+  end
+end
