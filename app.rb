adee5d84df751e32880cdd5e78eec6486e2c5172
kesleymartins <kesley.martins@vivaldi.net>
2023-08-13 22:50:51 -0300

    render index.html.erb with custom title

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
+require 'erb'
+
 class App
   def call(env)
     headers = {
       'Content-Type' => 'text/html'
     }
 
-    response_html = File.read('views/index.html')
-    
+    title = 'Ruby on Rails'
+    erb = ERB.new(html_template)
+    response_html = erb.result(binding)
+
     [200, headers, [response_html]]
+  end
+
+  def html_template
+    File.read('views/index.html.erb')
   end
 end
