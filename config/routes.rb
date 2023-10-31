ac1a3b6d26f7fc1090cf34fd65c0cac1fb73d475
kesleymartins <kesley.martins@vivaldi.net>
2023-10-31 00:02:37 -0300

    remove old routes and add endpoint to articles#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 require_relative '../router'
 
 Router.draw do
-  get('/') do |_env|
-    'Root Page'
-  end
-
-  get('/articles') do
-    'All articles'
-  end
-
-  get('/articles/1') do
-    'one article'
-  end
+  get 'articles/index'
 end
